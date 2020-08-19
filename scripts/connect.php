001f3bbaca2faab77eb6870dd3640963f5474597
Ivan Ivković <ivan.ivkovic1996@gmail.com>
2020-08-19 23:04:21 +0200

    Connect PHP

---
+++ scripts/connect.php
@@ -0,0 +1,12 @@
+﻿<?php
+    $dbservername = "localhost";
+    $dbusername   = "root";
+    $dbpassword   = "";
+    $dbname   = "itrans";
+
+    $db = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
+    
+    if($db->connect_error) {   
+        die("Error: " . $db->connect_error);
+    }
+?>
